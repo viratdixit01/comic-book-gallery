4388d9b094a1754800cebf7fad227580e0a3a8a3
viratdixit01 <viratdixit01@gmail.com>
2019-07-05 15:30:33 +0530

    Updated Comic Book Detail View to display updated information

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,11 +10,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if(DateTime.Today.DayOfWeek==DayOfWeek.Friday)
-            {
-                return Redirect("/");
-            }
-            return Content("Hello From Comic Book Controller");
+            return View();
         }
     }
 }
